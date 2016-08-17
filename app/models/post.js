371b7fe35bfcde0dbdf987312ea6faaa5c04e8ea
Caleb Stokka and Noah Ramey <noah@>
2016-08-17 11:16:50 -0700

    display all posts on the homepage.

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  author: DS.attr(),
+  content: DS.attr(),
+  title: DS.attr(),
+  category: DS.attr(),
+  image: DS.attr()
 });
--- config/environment.js
+++ config/environment.js
@@ -12,6 +12,13 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
+      
+    firebase: {
+      apiKey: "AIzaSyCsghHYGP5WPzQzGpLS1A6uptiTBIhMNok",
+      authDomain: "blog-ember-fbaa5.firebaseapp.com",
+      databaseURL: "https://blog-ember-fbaa5.firebaseio.com",
+      storageBucket: "blog-ember-fbaa5.appspot.com",
+    },
 
     APP: {
       // Here you can pass flags/options to your application instance
